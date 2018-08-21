cf3d848202a9621b999139ebed86682915ac70d2
Sergii, Putintsev <putintsev.sergii@accenture.com>
2018-08-21 20:09:29 +0300

    added simple test example

--- app/src/main/java/com/certification/putintsevsergii/certification/songsDetails/media/MusicManager.kt
+++ app/src/main/java/com/certification/putintsevsergii/certification/songsDetails/media/MusicManager.kt
@@ -10,11 +10,12 @@ class MusicManager(context: Context?) {
     private var mediaPlayer: MediaPlayer = MediaPlayer.create(context, Uri.parse("https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/AudioPreview125/v4/91/85/0f/91850f1d-85b8-1aa0-6eea-e5988b0eb194/mzaf_6254647395340771291.plus.aac.p.m4a"))
 
     fun start() {
+        if (!mediaPlayer.isPlaying)
         mediaPlayer.start()
     }
 
     fun pause() {
-        mediaPlayer.pause()
+         mediaPlayer.pause()
     }
 
     fun stop() {
--- app/src/test/java/com/certification/putintsevsergii/certification/ExampleUnitTest.kt
+++ app/src/test/java/com/certification/putintsevsergii/certification/ExampleUnitTest.kt
@@ -1,17 +1,58 @@
 package com.certification.putintsevsergii.certification
 
-import org.junit.Test
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import android.content.Context
+import com.certification.putintsevsergii.certification.database.AppDatabase
+import com.certification.putintsevsergii.certification.networking.NetworkManager
+import com.certification.putintsevsergii.certification.networking.data.ChartsResponse
+import com.certification.putintsevsergii.certification.networking.data.Feed
+import com.certification.putintsevsergii.certification.networking.data.Result
+import org.junit.*
 
-import org.junit.Assert.*
-
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import retrofit2.mock.Calls
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    @Rule
+    @JvmField
+    var rule = InstantTaskExecutorRule()
+
+    val arr = ArrayList<Result>()
+    val mockResp = ChartsResponse(Feed(arr))
+
+    private lateinit var vm: TopChartsViewModel
+    private lateinit var repo: TopChartsRepository
+    @Mock
+    lateinit var client: NetworkManager
+    @Mock
+    lateinit var database: AppDatabase
+
+    @Mock
+    private lateinit var mMockContext: Context
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+
+        Mockito.`when`(client.getTopSongCharts(10))
+                .thenReturn(Calls.response(mockResp))
+
+    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun testVmInitialized() {
+
+        repo = TopChartsRepository(client, database)
+        Assert.assertNotNull(repo)
+
+        val result = repo.fetchTopSongCarts(10)
+       Assert.assertEquals("Checks mock call successfull" , 0, result?.size )
     }
 }
